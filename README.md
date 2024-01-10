c9968c43a4fb513662f92964d64585b60530845e
belen-m-h <belen.munoz@enferm.io>
2024-01-10 20:56:45 +0000

    init

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # PRA_Visualizacion
 
 
-En esta segunda parte de la práctica, el alumno tendrá que desarrollar una visualización de datos que demuestre su conocimiento del campo, así como el uso de diferentes herramientas y técnicas, basadas en el conjunto de datos seleccionado y validado en la primera parte de la práctica.
+En esta segunda parte de la práctica, el alumno tendrá que desarrollar una visualización de 
+datos que demuestre su conocimiento del campo, así como el uso de diferentes herramientas y 
+técnicas, basadas en el conjunto de datos seleccionado y validado en la primera parte de la práctica.
 
 Esta práctica está orientada a los siguientes objetivos:
 
@@ -12,11 +14,13 @@ Comprender los elementos de interactividad que proporcionan valor a una visualiz
 Además de la visualización, el estudiante tendrá que entregar un video que describa los puntos siguientes:
 
 [20%] El proceso de creación seguido y las decisiones de diseño que se hayan tomado a lo largo del desarrollo.
-[20%] La presentación in situ de la visualización, es decir, comentar las características de la visualización mientras se navega.
+[20%] La presentación in situ de la visualización, es decir, comentar las características de la 
+    visualización mientras se navega.
 [15%] El conjunto de datos que se visualiza.
 [20%] Las preguntas que responde la visualización.
 [15%] Los elementos interactivos disponibles como parte de la visualización.
-[10%] Reflexión: ¿Qué he aprendido del conjunto de datos? ¿Qué he aprendido de las técnicas utilizadas? ¿Qué me habría gustado hacer y no pude / supe?
+[10%] Reflexión: ¿Qué he aprendido del conjunto de datos? ¿Qué he aprendido de las técnicas utilizadas? 
+    ¿Qué me habría gustado hacer y no pude / supe?
 
 El video debe durar entre 4 y 6 minutos. Hay que respetar este rango de tiempo, se penalizará superar los márgenes porque también se evalúa la capacidad de síntesis y la calidad del guión.
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('../data/Airbnb_Open_Data.csv')
 
@@ -38,7 +40,7 @@ cat_columns = ['host_identity_verified','instant_bookable','room type','cancella
               ,'review rate number','Construction year']
 num_columns = ['lat','long','minimum nights','number of reviews','reviews per month','price','service fee'
               ,'calculated host listings count','availability 365']
-
+df = df.dropna(subset=['neighbourhood group'])
 
 cat_df = df[cat_columns]
 from scipy.stats import chi2_contingency
@@ -59,3 +61,93 @@ plt.figure(figsize=(8, 4))
 sns.heatmap(chi2_contingency_table, annot=True, fmt=".2f", cmap='coolwarm')
 plt.title("Chi-Squared Test Results for Categorical Variables")
 plt.show()
+
+
+
+
+# Cuestiones referentes a las zonas geográficas:
+    # ¿Cómo se distribuyen geográficamente las propiedades de Airbnb en la ciudad de Nueva York?
+
+import folium
+from folium.plugins import MarkerCluster
+
+
+mapa = {
+'Brooklyn':[40.6782,-73.9442],
+'brookln':[40.6782,-73.9442],
+'Manhattan':[40.7831,-73.9712],
+'manhatan':[40.7831,-73.9712],
+'Queens':[40.7282,-73.7949],
+'Staten Island':[ 40.5795,-74.1502],
+'Bronx':[ 40.8448,-73.8648],
+}
+
+
+# coord_mapping = dict(zip(df['neighbourhood group'], zip(df['latitude'], df['longitude'])))
+
+# Añadir las columnas de latitud y longitud al DataFrame
+# df['latitude'] = mapa[df['neighbourhood group']][0]
+# df['longitude'] =  mapa[df['neighbourhood group']][1]
+
+df['latitude'] = df['neighbourhood group'].map(lambda x: mapa.get(x, [None, None])[0])
+df['longitude'] = df['neighbourhood group'].map(lambda x: mapa.get(x, [None, None])[1])
+
+
+# Visualizar las longitudes con los nuevos campos
+plt.figure(figsize=(10, 6))
+plt.scatter(df['longitude'], df['latitude'], c='blue', marker='o', label='Neighbourhood Group')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Mapa de Longitudes con Nuevos Campos')
+plt.legend()
+plt.show()
+
+
+
+import folium
+from folium.plugins import MarkerCluster
+
+# Supongamos que tienes un DataFrame llamado 'df' con datos geográficos
+# y una columna 'latitude' y 'longitude'
+
+# Crear un mapa centrado en las coordenadas promedio
+map_center = [df['latitude'].mean(), df['longitude'].mean()]
+mymap = folium.Map(location=map_center, zoom_start=10)
+
+# Agregar marcadores para cada casa
+marker_cluster = MarkerCluster().add_to(mymap)
+
+for index, row in df.iterrows():
+    folium.Marker([row['latitude'], row['longitude']]).add_to(marker_cluster)
+
+# Mostrar el mapa
+mymap.save('mapa_de_casas.html')
+
+
+
+
+    # ¿Se observan diferencias notables en precios y disponibilidad entre distintos vecindarios?
+
+plt.figure(figsize=(8, 4))
+sns.barplot(data=df, x='neighbourhood group', y='price', hue='room type')
+plt.xlabel('neighbourhood group')
+plt.ylabel('room type')
+plt.show()
+
+
+# Características de alojamientos:
+    # ¿Cuál es la distribución de tipos de propiedades?,¿ son casas enteras, habitaciones, pisos enteros,...?
+    # ¿Cómo afectan las características de los alojamientos en el precio?
+    # ¿Cuántos anfitriones tienen la distinción de super anfitrión?
+    # ¿Hay una correlación entre el rating de la propiedad y si la casa es de un super anfitrión?
+
+#  Calificaciones:
+    # ¿Cómo se distribuyen las calificaciones de las revisiones?
+    # ¿Hay correlaciones entre las diferentes categorías de calificaciones y el precio?
+
+# Temporalidad:
+    # ¿Existen patrones estacionales en los precios o en la ocupación de las propiedades?
+
+# Servicios de las propiedades:
+    # ¿Cuáles son los servicios más comunes ofrecidos por los anfitriones?
+    # ¿Existe una correlación entre la presencia de ciertos servicios y el precio?
